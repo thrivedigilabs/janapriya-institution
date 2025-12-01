e08c134571457ca779fbef719107e750f3fb3081
thrivedigilabs <thrivedigilabs@gmail.com>
2025-12-01 08:16:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/janapriya-institutions">
+      <BrowserRouter basename="/janapriya-institution">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
